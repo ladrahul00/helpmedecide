d11320db49829f8f4f9a2b4a17bea11e4b9accb7
Rahul Lad <ladrahul00@gmail.com>
2023-01-07 20:04:25 +0530

    helpmedecide app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+// import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  selectedOption: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  helpNeeded = false;
   title = 'helpmedecide';
+  listOfOptions: string[] = [];
+  newOption = new FormControl('');
+
+  // constructor(public dialog: MatDialog) { }
+  constructor(private _snackBar: MatSnackBar) { }
+
+  needHelp() {
+    this.helpNeeded = true;
+  }
+
+  addOption() {
+    const newOptionValue = this.newOption.value;
+    if (!newOptionValue || newOptionValue === "") {
+      return;
+    }
+    this.listOfOptions.push(newOptionValue);
+    this.newOption.reset();
+  }
+
+  pickAtRandom() {
+    const size = this.listOfOptions.length;
+    if (size === 0) {
+      console.log("empty shit")
+      this._snackBar.open("Give me some options bitch", "Ok", {
+        duration: 2000
+      });
+      return;
+    }
+    const selectedItem = Math.floor(Math.random() * size);
+
+    this._snackBar.open(this.listOfOptions[selectedItem], "Done", {
+      duration: 10000
+    });
+    // this.dialog.open(DialogDataExampleDialog, {
+    //   data: {
+    //     selectedOption: this.listOfOptions[selectedItem],
+    //   },
+    // });
+  }
 }
+
+// @Component({
+//   selector: 'show-selected',
+//   templateUrl: 'show-selected.html',
+// })
+// export class DialogDataExampleDialog {
+//   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSnackBarModule
+    // MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
